6d841714d57dbbebcfdd9c7eca160199899e491c
Lulu <92340210+lulu-sun@users.noreply.github.com>
2021-11-08 03:25:45 -0800

    Graphs (#7)
    * added test graph with CSS
    
    * added test graph
    
    * added 2 graphs test

--- src/app.js
+++ src/app.js
@@ -4,6 +4,49 @@
    window.addEventListener("load", init);
 
    function init() {
+     createGraph("graph1");
+     createGraph("graph2");
+   }
+
+   function createGraph(id) {
+    // let ctx = id("chart");
+
+   const myChart = new Chart(id, {
+       type: 'bar',
+       data: {
+           labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+           datasets: [{
+               label: '# of Votes',
+               data: [12, 19, 3, 5, 2, 3],
+               backgroundColor: [
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(54, 162, 235, 0.2)',
+                   'rgba(255, 206, 86, 0.2)',
+                   'rgba(75, 192, 192, 0.2)',
+                   'rgba(153, 102, 255, 0.2)',
+                   'rgba(255, 159, 64, 0.2)'
+               ],
+               borderColor: [
+                   'rgba(255, 99, 132, 1)',
+                   'rgba(54, 162, 235, 1)',
+                   'rgba(255, 206, 86, 1)',
+                   'rgba(75, 192, 192, 1)',
+                   'rgba(153, 102, 255, 1)',
+                   'rgba(255, 159, 64, 1)'
+               ],
+               borderWidth: 1
+           }]
+       },
+       options: {
+            responsive: false,
+            scales: {
+                y: {
+                    beginAtZero: true
+               }
+           }
+       }
+   });
+   }
 
      createGraph();
     //  id("start-btn").addEventListener("click", startGame);
@@ -49,42 +92,6 @@
        }
    });
    }
-
-  //  const ctx = document.getElementById('myChart');
-  //  const myChart = new Chart(ctx, {
-  //      type: 'bar',
-  //      data: {
-  //          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  //          datasets: [{
-  //              label: '# of Votes',
-  //              data: [12, 19, 3, 5, 2, 3],
-  //              backgroundColor: [
-  //                  'rgba(255, 99, 132, 0.2)',
-  //                  'rgba(54, 162, 235, 0.2)',
-  //                  'rgba(255, 206, 86, 0.2)',
-  //                  'rgba(75, 192, 192, 0.2)',
-  //                  'rgba(153, 102, 255, 0.2)',
-  //                  'rgba(255, 159, 64, 0.2)'
-  //              ],
-  //              borderColor: [
-  //                  'rgba(255, 99, 132, 1)',
-  //                  'rgba(54, 162, 235, 1)',
-  //                  'rgba(255, 206, 86, 1)',
-  //                  'rgba(75, 192, 192, 1)',
-  //                  'rgba(153, 102, 255, 1)',
-  //                  'rgba(255, 159, 64, 1)'
-  //              ],
-  //              borderWidth: 1
-  //          }]
-  //      },
-  //      options: {
-  //          scales: {
-  //              y: {
-  //                  beginAtZero: true
-  //              }
-  //          }
-  //      }
-  //  });
  
    /**
     * Returns a new element with the given tag name.
