edae02c2581b7471968950d1198f0a34fc3d77b6
brotatotes <erichao2018@gmail.com>
2021-11-11 01:54:02 -0800

    add graphs and calculate relevant data

--- src/services/RiotAPI.js
+++ src/services/RiotAPI.js
@@ -1,9 +1,8 @@
-// example api calls in another file:
 "use strict";
 
 const latestDataDragonVersion = "11.22.1";
 
-const KEY = "RGAPI-09f4ea9a-93f7-4d97-8bf2-53ce182db87e";
+const KEY = "RGAPI-38a9c896-4504-415f-b917-2b3221ed5434";
 const KEY_QUERY = "?api_key=" + KEY;
 
 const MAPPED_REGIONS = {"americas": ["na1", "br1", "la1", "la2", "oc1"],
@@ -17,6 +16,86 @@ const REQUEST_OPTIONS = {
 }
 
 /**
+ * One function to rule them all. 
+ * 
+ * A single function that calls all necessary helpers to aggregate and return 
+ * a single object with everything needed by the app.
+ * 
+ * This also prevents multiple helpers from each calling the same API multiple times.
+ * @param {String} region 
+ * @param {String} summonerName 
+ */
+export async function getUserData(region, summonerName) {
+    let resultObj = [];
+
+    let summoner = await getSummonerByName(region, summonerName);
+
+    let matches = await getMatches(region, summoner.puuid, "ranked", 10);
+
+    resultObj["gamesFound"] = matches.length;
+    if (resultObj["gamesFound"] < 1) {
+        return resultObj;
+    }
+
+    let matchInfos = await getMatchInfos(region, matches);
+
+    resultObj["summoner"] = summoner;
+
+    const gameStats = await getGameStats(summoner.puuid, matchInfos);
+    resultObj["gameStats"] = gameStats;
+
+    resultObj["killPercentage"] = getKillPercentage(gameStats.playerStats, gameStats.teamStats);
+
+    resultObj["deathPercentage"] = getDeathPercentage(gameStats.playerStats, gameStats.teamPlayerStats, gameStats.teamStats);
+
+    resultObj["damagePercentage"] = getDamagePercentage(gameStats.playerStats, gameStats.teamPlayerStats);
+
+    resultObj["killParticipationPercentage"] = getKillParticipationPercentage(gameStats.playerStats, gameStats.teamStats);
+
+    resultObj["minionsKilledPercentage"] = getMinionsKilledPercentage(gameStats.playerStats, gameStats.teamPlayerStats, gameStats.teamStats);
+
+    resultObj["goldEarnedPercentage"] = getGoldEarnedPercentage(gameStats.playerStats, gameStats.teamPlayerStats, gameStats.teamStats);
+
+    resultObj["visionScorePerMinute"] = getVisionScorePerMinute(gameStats.playerStats, gameStats.gameTimes);
+
+    resultObj["visionScorePercentage"] = getVisionScorePercentage(gameStats.playerStats, gameStats.teamPlayerStats, gameStats.teamStats);
+
+    return resultObj;
+}
+
+async function getGameStats(puuid, matchInfos) {
+    let resultObj = [];
+
+    let gameTimes = [];
+    let teamPlayerStats = [];
+    let playerStats = [];
+    let teamStats = [];
+    for (const matchInfo of matchInfos) {
+        let playerGameTimes = [];
+
+        teamPlayerStats.push(matchInfo.info.participants);
+        for (const participant of matchInfo.info.participants) {
+            playerGameTimes.push(participant.timePlayed);
+
+            if (participant.puuid === puuid) {
+                playerStats.push(participant);
+                teamStats.push(matchInfo.info.teams[(participant.teamId / 100) - 1]);
+                break;
+            }
+        }
+
+        gameTimes.push(Math.max.apply(Math, playerGameTimes));
+    }
+
+    resultObj["gameTimes"] = gameTimes;
+    resultObj["teamPlayerStats"] = teamPlayerStats;
+    resultObj["playerStats"] = playerStats;
+    resultObj["teamStats"] = teamStats;
+
+    return resultObj
+}
+
+/**
  * Example API (check if it works)
  * gets champion rotations
  *
@@ -24,7 +103,7 @@ const REQUEST_OPTIONS = {
  * @param {String} region ex: NA1, BR1, EWN1, EWN1, JP1, KR, LA1, LA2, OC1, RU, TR1
  * @returns maxNewPlayerLevel, freeChampionIdsForNewPlayers, freeChampionIds
  */
-export function champRotation(region) {
+async function getChampRotation(region) {
     return fetch('https://' + region + '.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=' + KEY_QUERY, REQUEST_OPTIONS)
         .then(resp => resp.json())
         .catch((error) => console.warn("ERROR: ", error));
@@ -36,15 +115,15 @@ export function champRotation(region) {
  * @param {string} name
  * @returns object with summoner name and puuid.
  */
-export async function summonerByName(region, name) {
+async function getSummonerByName(region, name) {
     try {
         let response = await fetch('https://' + region + '.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + name + KEY_QUERY, REQUEST_OPTIONS);
         await statusCheck(response);
         let data = await response.json();
-        console.log(data);
         return {"name": name, "puuid": data.puuid, 
             "profileIcondId": data.profileIconId, "summonerLevel": data.summonerLevel};
     } catch(e) {
+        console.warn(e);
         return "Error: Could not find player name";
     }
 }
@@ -65,97 +144,86 @@ function findGeneralRegion(region) {
     }
     return generalRegion;
 }
+
 /**
  *
  * @param {string} region
  * @param {string} name
+ * @param {string} matchType
  * @returns the match data of last 10 ranked games, summoner name, and summoner puuid
  */
-async function getMatches(region, name) {
+async function getMatches(region, puuid, matchType, count) {
     try {
-        let summonerName = await summonerByName(region, name);
-
-        let puuid = summonerName.puuid;
-        // number of games ; range: 1-100
-        let count = 10;
-        let type = "ranked";
         let queryString ='https://' + findGeneralRegion(region) + '.api.riotgames.com/lol/match/v5/matches/by-puuid/' + puuid + '/ids' + KEY_QUERY;
         queryString += "&count=" + count;
-        queryString += "&type=" + type;
-
+        queryString += "&type=" + matchType;
         let response = await fetch(queryString, REQUEST_OPTIONS);
         await statusCheck(response);
         let data = await response.json();
 
-        let returnData = summonerName;
-        returnData["matches"] = data;
-        return returnData;
+        return data
     } catch(e) {
-        console.log(e);
+        console.warn(e);
         return "Error: Could not find matches for specified player";
     }
 }
+
 /**
  *
  * @param {string} region
- * @param {string} name
- * @returns player stats, general team stats, team player stats, and match ids of the last {10} games. (see getMatchs())
- * also returns with summoner name and puuid
+ * @param {string[]} matches
+ * @returns array of matchInfo
  */
-export async function getMatchesInfo(region, name) {
+async function getMatchInfos(region, matches) {
     try {
-        let arr = [];
-        let team = [];
-        let teamPlayerStats = [];
-        let json = await getMatches(region, name);
-        json["gamesFound"] = !(json.matches.length === 0)
-        let gameTime = [];
-        for (const matchId of json.matches) {
-            let allPlayerGameTime = []
+        let matchInfos = [];
+        for (const matchId of matches) {
             let response = await fetch('https://' + findGeneralRegion(region) + '.api.riotgames.com/lol/match/v5/matches/' + matchId + KEY_QUERY);
             await statusCheck(response);
-            let data = await response.json();
-            teamPlayerStats.push(data.info.participants);
-            for (const participant of data.info.participants) {
-                allPlayerGameTime.push(participant.timePlayed);
-                if (participant.puuid === json.puuid) {
-                    arr.push(participant);
-                    team.push(data.info.teams[(participant.teamId / 100) - 1]);
-                    break;
-                }
-            }
-            let maxGameTime = Math.max.apply(Math, allPlayerGameTime);
-            gameTime.push(maxGameTime / 60);
+            let matchInfo = await response.json();
+            matchInfos.push(matchInfo);
         }
-        json["gameTime"] = gameTime;
-        json["teamPlayerStats"] = teamPlayerStats;
-        json["playerStats"] = arr;
-        json["teamStats"] = team;
-        return json;
+
+        return matchInfos;
     } catch(e) {
+        console.warn(e);
         return "Error: Could not get match info for specified player";
     }
 }
 
+function getKillPercentage(playerStats, teamStats) {
+    let killPercentages = [];
+    for (let i = 0; i < 10; i++) {
+      killPercentages.push((playerStats[i].kills / teamStats[i].objectives.champion.kills)* 100);
+    }
+
+    return median(killPercentages);
+}
+
+function median(arr) {
+    const mid = Math.floor(arr.length / 2);
+    const nums = [...arr].sort((a, b) => a - b);
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2
+}
+
 /**
  *
  * @param {string} region
- * @param {string} name
+ * @param {string} matchInfos
  * @returns an array of ally team's total damage dealt to champions ({10} games).
  * Each index represents one game. (From most recent ranked games)
  */
-export async function getMatchesDmgPercentage(region, name) {
-    let playerMatchData = await getMatchesInfo(region, name);
-    let gameDmg = [];
+function getDamagePercentage(playerStats, teamPlayerStats) {
+    let gameDamages = [];
     let index = 0;
     let playerTeamIds = [];
     let playerDmgDealt = [];
-    for (const playerMatch of playerMatchData.playerStats) {
+    for (const playerMatch of playerStats) {
         playerTeamIds.push(playerMatch.teamId);
         playerDmgDealt.push(playerMatch.totalDamageDealtToChampions);
     }
 
-    for (const match of playerMatchData.teamPlayerStats) {
+    for (const match of teamPlayerStats) {
         let summonerTeam = playerTeamIds[index];
         let tally = 0;
         for (const players of match) {
@@ -163,11 +231,10 @@ export async function getMatchesDmgPercentage(region, name) {
                 tally += players.totalDamageDealtToChampions;
             }
         }
-        gameDmg.push(playerDmgDealt[index] / tally * 100);
+        gameDamages.push(playerDmgDealt[index] / tally * 100);
         index++;
     }
-    return gameDmg;
-
+    return median(gameDamages);
 }
 
 /**
@@ -176,29 +243,170 @@ export async function getMatchesDmgPercentage(region, name) {
  * @param {string} name 
  * @returns array of vision score per minute 0 index is the most recent ranked game
  */
-export async function getVisionScorePerMinute(region, name) {
-    let playerMatchData = await getMatchesInfo(region, name);
+function getVisionScorePerMinute(playerStats, gameTimes) {
     let visionScorePerMinute = [];
     let visionScores = []
     let index = 0;
-    for (const playerStat of playerMatchData.playerStats) {
+    for (const playerStat of playerStats) {
         visionScores.push(playerStat.visionScore);
     }
-    for (const gameDuration of playerMatchData.gameTime) {
-        visionScorePerMinute.push(visionScores[index] / gameDuration);
+
+    for (const gameTime of gameTimes) {
+        if (gameTime !== 0) {
+            visionScorePerMinute.push(visionScores[index] / gameTime * 60);
+        }
         index += 1;
     }
-    return visionScorePerMinute;
+
+    return median(visionScorePerMinute);
 }
+
+function getVisionScorePercentage(playerStats, teamPlayerStats, teamStats) {
+    let playerVisionScores = [];
+    for (const playerStat of playerStats) {
+        playerVisionScores.push(playerStat.visionScore);
+    }
+
+    let teamVisionScores = [];
+    for (let i = 0; i < teamPlayerStats.length; i++) {
+        let teamVisionScore = 0;
+        for (const singlePlayerStat of teamPlayerStats[i]) {
+            if (singlePlayerStat.teamId === teamStats[i].teamId) {
+                teamVisionScore += singlePlayerStat.visionScore;
+            }
+        }
+
+        teamVisionScores.push(teamVisionScore);
+    }
+
+    let percentages = [];
+    for (let i = 0; i < playerVisionScores.length; i++) {
+        if (teamVisionScores[i] !== 0) {
+            percentages.push(playerVisionScores[i] / teamVisionScores[i] * 100);
+        }
+    }
+
+    return median(percentages);
+}
+
+function getKillParticipationPercentage(playerStats, teamStats) {
+    let playerKPs = [];
+    for (const playerStat of playerStats) {
+        playerKPs.push(playerStat.assists + playerStat.kills);
+    }
+
+    let teamKPs = [];
+    for (const teamStat of teamStats) {
+        teamKPs.push(teamStat.objectives.champion.kills);
+    }
+
+    let percentages = [];
+    for (let i = 0; i < playerKPs.length; i++) {
+        if (teamKPs[i] !== 0) {
+            percentages.push(playerKPs[i] / teamKPs[i] * 100);
+        }
+    }
+
+    return median(percentages)
+}
+
+
+function getMinionsKilledPercentage(playerStats, teamPlayerStats, teamStats) {
+    let playerCSs = [];
+    for (const playerStat of playerStats) {
+        playerCSs.push(playerStat.neutralMinionsKilled + playerStat.totalMinionsKilled);
+    }
+
+    let teamCSs = [];
+    for (let i = 0; i < teamPlayerStats.length; i++) {
+        let teamCS = 0;
+        for (const singlePlayerStat of teamPlayerStats[i]) {
+            if (singlePlayerStat.teamId === teamStats[i].teamId) {
+                teamCS += singlePlayerStat.neutralMinionsKilled + singlePlayerStat.totalMinionsKilled;
+            }
+        }
+
+        teamCSs.push(teamCS);
+    }
+
+    let percentages = [];
+    for (let i = 0; i < playerCSs.length; i++) {
+        if (teamCSs[i] !== 0) {
+            percentages.push(playerCSs[i] / teamCSs[i] * 100);
+        }
+    }
+
+    return median(percentages);
+}
+
+
+function getGoldEarnedPercentage(playerStats, teamPlayerStats, teamStats) {
+    let playerGolds = [];
+    for (const playerStat of playerStats) {
+        playerGolds.push(playerStat.goldEarned);
+    }
+
+    let teamGolds = [];
+    for (let i = 0; i < teamPlayerStats.length; i++) {
+        let teamGold = 0;
+        for (const singlePlayerStat of teamPlayerStats[i]) {
+            if (singlePlayerStat.teamId === teamStats[i].teamId) {
+                teamGold += singlePlayerStat.goldEarned;
+            }
+        }
+
+        teamGolds.push(teamGold);
+    }
+
+    let percentages = [];
+    for (let i = 0; i < playerGolds.length; i++) {
+        if (teamGolds[i] !== 0) {
+            percentages.push(playerGolds[i] / teamGolds[i] * 100);
+        }
+    }
+
+    return median(percentages);
+}
+
+function getDeathPercentage(playerStats, teamPlayerStats, teamStats) {
+    let playerDeaths = [];
+    for (const playerStat of playerStats) {
+        playerDeaths.push(playerStat.deaths);
+    }
+
+    let teamDeaths = [];
+    for (let i = 0; i < teamPlayerStats.length; i++) {
+        let teamDeath = 0;
+        for (const singlePlayerStat of teamPlayerStats[i]) {
+            if (singlePlayerStat.teamId === teamStats[i].teamId) {
+                teamDeath += singlePlayerStat.deaths;
+            }
+        }
+
+        teamDeaths.push(teamDeath);
+    }
+
+    console.log(playerDeaths);
+    console.log(teamDeaths);
+
+    let percentages = [];
+    for (let i = 0; i < playerDeaths.length; i++) {
+        if (teamDeaths[i] !== 0) {
+            percentages.push(playerDeaths[i] / teamDeaths[i] * 100);
+        }
+    }
+
+    return median(percentages);
+}
+
 /**
  * 
  * @param {string} region 
  * @param {string} name 
  * @returns object with profileIconLink and summonerLevel
  */
-export async function getProfileInfo(region, name) {
-    let summonerInfo = await summonerByName(region, name);
-    let id = summonerInfo.profileIcondId;
+function getProfileInfo(profileIcondId) {
+    let id = profileIcondId;
     let profileIconLink ='https://ddragon.leagueoflegends.com/cdn/' + latestDataDragonVersion + '/img/profileicon/' + id + '.png';
     let summonerLevel = summonerInfo.summonerLevel;
     return {"profileIconLink": profileIconLink, "summonerLevel": summonerLevel};
