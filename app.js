5ee46bf5fea37370080f468d3c14f7b2911c2e1c
Derzan Chiang <76019114+MiTo0o@users.noreply.github.com>
2021-11-12 15:31:01 -0800

    put api keyback in cuz imma clown

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-
 app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
