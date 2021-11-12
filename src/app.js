899f9d816b3e895ca3cc112627e0eefd702c6c1e
Evan <hao.amazing@gmail.com>
2021-11-12 14:29:28 -0800

    fixed bug and truncated

--- src/app.js
+++ src/app.js
@@ -125,6 +125,11 @@ import * as RiotAPI from "./services/RiotAPI.js";
     let figure = gen("figure");
     figure.appendChild(p);
 
+    // adjusts perfect integers to integers. Ex: 10.0 -> 10
+    if (score % 1 == 0) {
+      score = parseInt(score);
+    }
+
     p.innerHTML = "<h3>" + name + " Score: " + score + "/10\n\n"  + "</h3> " + comment ;
 
     id(container).appendChild(figure);
